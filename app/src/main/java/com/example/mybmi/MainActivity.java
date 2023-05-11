734887edfa59bc392851109d234b384e985ad301
goodsuper <givemejapan@gmail.com>
2023-05-11 22:39:32 +0800

    test

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initView();
+        int b = 0;
     }
 
     private void initView() {
